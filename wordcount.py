51333d000dc5e31a275a712b4f49f18ce7cc1a98
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 13:04:57 -0800

    Improved the Output... Made Program More Flexible...

--- wordcount.py
+++ wordcount.py
@@ -1,11 +1,19 @@
 # put your code here.
+import string
+import sys
+
+#print sys.argv
+
 def word_count(filename):
     """Taking in file, putting each unique word into a dictionary, and number of occurence as it's value"""
+    
+
     the_file = open(filename)
     word_count = {}
     for line in the_file:
-        line = line.rstrip().split(" ")
-        for word in line:
+        words = line.rstrip().split(" ")
+        for word in words:
+            word = word.strip(string.punctuation).lower()
             word_count[word] = word_count.get(word, 0) + 1
         #print word_count
     
@@ -13,4 +21,6 @@ def word_count(filename):
         print word, word_count[word]
     return word_count
 
-word_count("test.txt")
+
+for text_file in sys.argv[1:]:
+    word_count(text_file)
